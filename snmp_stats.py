35f408a4f389eeb66ca7925b8f66be5bd2f4a29c
Julian Metzler <git@mezgr.de>
2021-12-28 04:30:03 +0100

    SNMP Stats: Add error handling

--- snmp_stats.py
+++ snmp_stats.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 
 from pyfis.ibis import SerialIBISMaster, TCPIBISMaster
 from easysnmp import Session
@@ -18,21 +19,26 @@ def main():
     prev_in_bytes = 0
     prev_out_bytes = 0
     while True:
-        in_bytes = int(snmp.get(IF_HC_IN_OCTETS).value)
-        out_bytes = int(snmp.get(IF_HC_OUT_OCTETS).value)
+        try:
+            in_bytes = int(snmp.get(IF_HC_IN_OCTETS).value)
+            out_bytes = int(snmp.get(IF_HC_OUT_OCTETS).value)
 
-        now = time.time()
+            now = time.time()
 
-        in_rate = (in_bytes-prev_in_bytes) / (now - prev_time)
-        out_rate = (out_bytes-prev_out_bytes) / (now - prev_time)
+            in_rate = (in_bytes-prev_in_bytes) / (now - prev_time)
+            out_rate = (out_bytes-prev_out_bytes) / (now - prev_time)
 
-        prev_time = now
-        prev_in_bytes = in_bytes
-        prev_out_bytes = out_bytes
+            prev_time = now
+            prev_in_bytes = in_bytes
+            prev_out_bytes = out_bytes
 
-        ibis.DS009("Down: {:.1f} MB\n{:.2f} MB/s\nUp: {:.1f} MB\n{:.2f} MB/s".format(in_bytes/1048576, in_rate/1048576, out_bytes/1048576, out_rate/1048576))
-
-        time.sleep(5)
+            ibis.DS009("Down: {:.1f} MB\n{:.2f} MB/s\nUp: {:.1f} MB\n{:.2f} MB/s".format(in_bytes/1048576, in_rate/1048576, out_bytes/1048576, out_rate/1048576))
+            time.sleep(5)
+        except KeyboardInterrupt:
+            break
+        except:
+            traceback.print_exc()
+            time.sleep(5)
 
 
 
